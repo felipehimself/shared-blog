9859cc7d744be77b42c4ecd2412a609c1dd364f3
Felipe Couto <felipecoutodarocha@gmail.com>
2023-01-07 15:55:08 -0300

    feat: added package to handle database connection

---
+++ backend/src/database/database.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+	"shared-blog-backend/src/config"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Connect() (*sql.DB, error) {
+
+	db, err := sql.Open("mysql", config.ConnectionString)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+
+}
