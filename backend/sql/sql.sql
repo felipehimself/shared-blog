9b1ed1f12368ea109c229b5f58dd79f298f26600
Felipe Couto <felipecoutodarocha@gmail.com>
2023-01-08 20:15:11 -0300

    build: added sql file to assist with the creation of the tables

---
+++ backend/sql/sql.sql
@@ -0,0 +1,62 @@
+CREATE TABLE users (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)  ENGINE=INNODB
+
+CREATE TABLE posts (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    subtitle VARCHAR(120) NOT NULL,
+    author_id INT NOT NULL,
+    FOREIGN KEY (author_id)
+        REFERENCES users (id)
+        ON DELETE CASCADE,
+    content TEXT NOT NULL,
+    votes_number INT DEFAULT 0,
+    comments_number INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)  ENGINE=INNODB
+
+CREATE TABLE topics (
+	id	INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    topic VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)  ENGINE=INNODB
+
+CREATE TABLE post_topics (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    topic_id int NOT NULL,
+    FOREIGN KEY (topic_id)
+        REFERENCES topics (id)
+        ON DELETE CASCADE,
+
+    post_id INT NOT NULL,
+    FOREIGN KEY (post_id)
+        REFERENCES posts (id)
+        ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)  ENGINE=INNODB;
+
+
+
+insert into topics (topic) 
+values 
+("Blockchain"),
+("Carrer"),
+("Cloud"),
+("Databases"),
+("Data Science"),
+("Developer tools"),
+("DevOps & SRE"),
+("DevRel"),
+("Fundamentls"),
+("Gaming"),
+("Mobile Development"),
+("Programming language"),
+("Security"),
+("Testing"),
+("Web Development")
